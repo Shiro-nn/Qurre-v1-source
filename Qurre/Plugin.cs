2f385fe1fc01f52f98a7e74ff4ea24bcb2478b00
!Vo18#8763 <71818665+Vo18@users.noreply.github.com>
2020-12-11 04:22:33 +0300

    Update Plugin.cs

--- Qurre/Plugin.cs
+++ Qurre/Plugin.cs
@@ -1,13 +1,9 @@
-﻿namespace Qurre
+namespace Qurre
 {
 	public abstract class Plugin
 	{
 		public static YamlConfig Config;
 		public abstract string Name { get; }
-		
-		public abstract string Version { get; }
-		
-		public abstract string Developer { get; }
 		public abstract void Enable();
 		public abstract void Disable();
 		public abstract void Reload();
