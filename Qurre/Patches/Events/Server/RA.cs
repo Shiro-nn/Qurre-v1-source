ada8e3ce27d6a2a98ddb856ac65ce6dcab8352f7
fydne <serverafydne@gmail.com>
2020-12-23 17:08:06 +0500

    23.12.2020 15:08

--- Qurre/Patches/Events/Server/RA.cs
+++ Qurre/Patches/Events/Server/RA.cs
@@ -3,29 +3,31 @@ using HarmonyLib;
 using Qurre.API.Events;
 using RemoteAdmin;
 using System;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using static QurreModLoader.umm;
 namespace Qurre.Patches.Events.Server
 {
-	[HarmonyPatch(typeof(CommandProcessor), "ProcessQuery", new Type[]
-	{
-		typeof(string),
-		typeof(CommandSender)
-	})]
+	[HarmonyPatch(typeof(CommandProcessor), nameof(CommandProcessor.ProcessQuery), new Type[] { typeof(string), typeof(CommandSender) })]
 	internal static class RA
 	{
 		private static bool Prefix(ref string q, ref CommandSender sender)
 		{
-			var ev = new SendingRAEvent(sender, q);
-			PreauthStopwatch().Restart();
-			IdleMode.SetIdleMode(false);
-			if (q == "REQUEST_DATA PLAYER_LIST SILENT")
+			try
+			{
+				var ev = new SendingRAEvent(sender, q);
+				PreauthStopwatch().Restart();
+				IdleMode.SetIdleMode(false);
+				if (q == "REQUEST_DATA PLAYER_LIST SILENT")
+					return true;
+				Qurre.Events.Server.sendingra(ev);
+				if (!string.IsNullOrEmpty(ev.ReplyMessage))
+					sender.RaReply(ev.ReplyMessage, ev.Success, true, string.Empty);
+				return ev.IsAllowed;
+			}
+			catch (Exception e)
+			{
+				Log.Error($"umm, error in patching Server.RA:\n{e}\n{e.StackTrace}");
 				return true;
-			Qurre.Events.Server.sendingra(ev);
-			if (!string.IsNullOrEmpty(ev.ReplyMessage))
-				sender.RaReply(ev.ReplyMessage, ev.Success, true, string.Empty);
-			return ev.IsAllowed;
+			}
 		}
-    }
+	}
 }
