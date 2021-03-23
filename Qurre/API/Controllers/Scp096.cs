6d362c3ef121f3c72eeb7aa675cd3351097b72e2
fydne <54745523+fydne@users.noreply.github.com>
2021-03-23 23:51:09 +0500

    21:51 23.03.2021

--- Qurre/API/Controllers/Scp096.cs
+++ Qurre/API/Controllers/Scp096.cs
@@ -133,31 +133,6 @@ namespace Qurre.API.Controllers
             if (!Is096) return;
             Scp.Scp096_targets().Remove(player.ReferenceHub);
         }
-        public bool IsTarget
-        {
-            get
-            {
-                if (Targets.Contains(player))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            set
-            {
-                if (Targets.Contains(player))
-                {
-                    return;
-                }
-                else
-                {
-                    Targets.Add(player);
-                }
-            }
-        }
         public void ChargeDoor(Door door)
         {
             if (!Is096) return;
