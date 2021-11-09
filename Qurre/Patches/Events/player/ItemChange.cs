1eaddd31b505d15f0999362ddd33dd1af5fed1fd
fydne <serverafydne@gmail.com>
2021-11-09 07:54:29 +0500

    1.9.7

--- Qurre/Patches/Events/player/ItemChange.cs
+++ Qurre/Patches/Events/player/ItemChange.cs
@@ -1,20 +1,19 @@
 ﻿using HarmonyLib;
 using InventorySystem;
-using InventorySystem.Items;
 using Qurre.API;
 using Qurre.API.Controllers;
 using Qurre.API.Events;
 namespace Qurre.Patches.Events.player
 {
-    [HarmonyPatch(typeof(Inventory), nameof(Inventory.CurInstance), MethodType.Setter)]
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.ServerSelectItem))]
     internal static class ItemChange
     {
-        internal static bool Prefix(Inventory __instance, ItemBase value)
+        internal static bool Prefix(Inventory __instance, ushort itemSerial)
         {
             try
             {
-                if (__instance.CurInstance == value) return false;
-                var ev = new ItemChangeEvent(Player.Get(__instance._hub), Item.Get(__instance.CurInstance), Item.Get(value));
+                if (itemSerial == __instance.CurItem.SerialNumber) return false;
+                var ev = new ItemChangeEvent(Player.Get(__instance._hub), Item.Get(__instance.CurInstance), itemSerial == 0 ? null : Item.Get(itemSerial));
                 Qurre.Events.Invoke.Player.ItemChange(ev);
                 return ev.Allowed;
             }
