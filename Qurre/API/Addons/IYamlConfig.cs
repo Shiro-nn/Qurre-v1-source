d51c334c91bb7dd94edd67a707ad0b8c30254f3d
AlexanderK <73738345+AlexanderK666@users.noreply.github.com>
2021-12-20 22:55:12 +0300

    🏆 Upgraded Qurre.csproj

---
+++ Qurre/API/Addons/IYamlConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Qurre.API.Addons
+{
+    /// <summary>
+    /// The main configuration variables.
+    /// </summary>
+    public interface IYamlConfig
+    {
+        /// <summary>
+        /// Enable the plugin?
+        /// </summary>
+        bool IsEnabled { get; set; }
+    }
+}
