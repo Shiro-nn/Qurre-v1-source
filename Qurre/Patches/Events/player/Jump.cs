93f29dd2f30c7eda543a7b3f72289d3b56f4c6ea
G-Man <69349110+EmblyOne@users.noreply.github.com>
2022-02-15 14:22:49 +0200

    Add files via upload

---
+++ Qurre/Patches/Events/player/Jump.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Qurre.API.Events;
+using Qurre.API;
+
+namespace Qurre.Patches.Events.player
+{
+    [HarmonyPatch(typeof(PlayerMovementSync), nameof(PlayerMovementSync.ReceivePosition2DJump), typeof(ReferenceHub))]
+    internal static class Jump  
+    {
+        internal static bool Prefix(PlayerMovementSync __instance, ReferenceHub ply, PositionMessage2DJump position)
+        {
+            try
+            {
+                var ev = new JumpEvent(Player.Get(ply));
+                Qurre.Events.Invoke.Player.Jump(ev);
+                var pl = Player.Get(ply);
+                __instance._hub.playerMovementSync.ReceivePosition2D(position.Position, true);
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                Log.Error($"ummmm, error in patching Player[Jump]:{e}\n{e.StackTrace}");
+                return true; 
+            }
+          
+        }
+    }
+}
