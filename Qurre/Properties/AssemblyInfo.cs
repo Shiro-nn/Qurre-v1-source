2bed41f7201b8f0242706b39ed6b34792d9ab75a
milusocool <43803886+milusocool@users.noreply.github.com>
2022-03-22 17:55:46 +0800

    Update AssemblyInfo.cs
    Wrong AssemblyInfo
    Without " at the end of line

--- Qurre/Properties/AssemblyInfo.cs
+++ Qurre/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/Qurre-Team/Qurre-sl")]
 [assembly: AssemblyCompany("Qurre Team")]
 [assembly: AssemblyProduct("Qurre")]
-[assembly: AssemblyCopyright("Copyright © Qurre Team 2020 - 2022)]
+[assembly: AssemblyCopyright("Copyright © Qurre Team 2020 - 2022")]
 [assembly: AssemblyTrademark("Qurre Team")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e1cb0906-b953-41e5-a594-11f65a361ef8")]
