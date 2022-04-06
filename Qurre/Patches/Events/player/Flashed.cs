0075da1935bf7aca9b7f6b2bb7ba70ff67a99a54
milusocool <43803886+milusocool@users.noreply.github.com>
2022-04-06 22:51:24 +0800

    Fix
    A Fix about Flashed distance

--- Qurre/Patches/Events/player/Flashed.cs
+++ Qurre/Patches/Events/player/Flashed.cs
@@ -19,8 +19,18 @@ namespace Qurre.Patches.Events.player
 				Vector3 vector = __instance.transform.position - hub.PlayerCameraReference.position;
 				float num = vector.magnitude;
 				float num2 = __instance._deafenDurationOverDistance.Evaluate(num);
-				if (num2 > __instance._minimalEffectDuration) target.EnableEffect<Deafened>(num2, true);
-				if (hub.transform.position.y > 900f) num /= __instance._surfaceZoneDistanceIntensifier;
+				if (num2 > __instance._minimalEffectDuration)
+				{
+					hub.playerEffectsController.EnableEffect<CustomPlayerEffects.Deafened>(num2, true);
+				}
+				if (Physics.Linecast(__instance.transform.position, hub.PlayerCameraReference.position, __instance._blindingMask))
+				{
+					return false;
+				}
+				if (hub.transform.position.y > 900f)
+				{
+					num /= __instance._surfaceZoneDistanceIntensifier;
+				}
 				float num3 = __instance._blindingOverDistance.Evaluate(num) * __instance._blindingOverDot.Evaluate(Vector3.Dot(hub.PlayerCameraReference.forward, vector.normalized));
 				bool allowed = false;
 				if (num3 > __instance._minimalEffectDuration) allowed = true;
