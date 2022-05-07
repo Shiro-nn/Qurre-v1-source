829df39b95ba07c9630f717b0a0d2097ebbf5eef
fydne <serverafydne@gmail.com>
2022-05-07 21:35:24 +0500

    ff fix

--- Qurre/Events/Modules/Etc.cs
+++ Qurre/Events/Modules/Etc.cs
@@ -71,7 +71,8 @@ namespace Qurre.Events.Modules
         {
             if (API.Server.FriendlyFire || ev.Target is null || ev.Attacker is null || ev.Attacker.FriendlyFire)
                 return;
-            ev.FriendlyFire = ev.Target.Side == ev.Attacker.Side;
+            if (ev.Target == ev.Attacker && ev.PrimitiveType == DamageTypesPrimitive.Explosion) ev.FriendlyFire = false;
+            else ev.FriendlyFire = ev.Target.Side == ev.Attacker.Side;
         }
         private static void ChangeRole(RoleChangeEvent ev)
         {
